606c8d90a8957085b31df0250d123fa8cd3208c8
Pinn <138990007+p-inn@users.noreply.github.com>
2024-08-12 23:19:41 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.53 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+    let answer = '';
+
+    for (let i = 0; i < my_string.length; i++) {
+        let char = my_string[i];
+        
+        if (char === char.toUpperCase()) {
+            answer += char.toLowerCase();
+        } else {
+            answer += char.toUpperCase();
+        }
+    }
+
+    return answer;
+}
