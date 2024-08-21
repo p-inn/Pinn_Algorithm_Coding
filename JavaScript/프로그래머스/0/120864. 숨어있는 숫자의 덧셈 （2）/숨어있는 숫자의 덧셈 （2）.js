ddadef2c2ce6b542174c91020a12e0fb367a1dec
Pinn <138990007+p-inn@users.noreply.github.com>
2024-08-21 21:37:51 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+
+    const numbers = my_string.match(/\d+/g);
+
+    if (!numbers) return 0;
+
+    const sum = numbers.map(Number).reduce((acc, curr) => acc + curr, 0);
+    
+    return sum;
+}
