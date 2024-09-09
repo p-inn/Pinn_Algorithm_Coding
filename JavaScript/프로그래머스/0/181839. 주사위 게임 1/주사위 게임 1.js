10a5c14d501010b8e5cb426f83f907e71746300b
Pinn <138990007+p-inn@users.noreply.github.com>
2024-09-09 13:45:38 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,14 @@
+function solution(a, b) {
+    const isOdd = (n) => n % 2 !== 0;
+    
+    if (isOdd(a) && isOdd(b)) {
+
+        return a * a + b * b;
+    } else if (isOdd(a) || isOdd(b)) {
+
+        return 2 * (a + b);
+    } else {
+
+        return Math.abs(a - b);
+    }
+}
