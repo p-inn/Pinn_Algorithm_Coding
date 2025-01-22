62a415dddcdbc120dba05440bfbc399de4b3d774
Pinn <138990007+p-inn@users.noreply.github.com>
2025-01-22 11:57:23 +0900

    [level 0] Title: 두 수의 합, Time: 100.86 ms, Memory: 34.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ JavaScript/프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,17 +1,4 @@
-function solution(a, b) {
-    let carry = 0;
-    let result = "";
-    let i = a.length - 1, j = b.length - 1;
+const solution = (a, b) => String(BigInt(a) + BigInt(b))
 
-    while (i >= 0 || j >= 0 || carry > 0) {
-        const digitA = i >= 0 ? +a[i--] : 0;
-        const digitB = j >= 0 ? +b[j--] : 0; 
-        const sum = digitA + digitB + carry;
-        carry = Math.floor(sum / 10); 
-        result = (sum % 10) + result;
-    }
-
-    return result;
-}
 
 
