3156a9f5da2a1d86d0d301f950b780a3a466c8b8
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-11 20:46:28 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ JavaScript/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,9 +1,4 @@
 function solution(num1, num2) {
-    var answer = 0;
-    if(num1===num2){
-        answer = 1;
-    }else{
-        answer = -1;
-    }
-    return answer;
+    if(num1 == num2) return 1;
+    else return -1;
 }
