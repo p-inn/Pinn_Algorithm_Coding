74d65da06cb36270a2300483c6e71533b6769f76
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-11 21:04:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ JavaScript/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,3 +1,7 @@
 function solution(numbers) {
-    return numbers.map(number => number * 2);
+    var answer = [];
+    for (let i =0; i < numbers.length; i++){
+        answer.push(numbers[i] * 2);
+    }
+    return answer;
 }
