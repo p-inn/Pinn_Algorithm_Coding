5449711c46193178d32ab174776b11137a71292c
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-11 20:44:43 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ JavaScript/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -1,6 +1,4 @@
 function solution(num1, num2) {
-    var answer1 = (num1/num2)*1000;
-    var answer = Math.floor(answer1);
-    
-    return answer;
+    var answer = num1 / num2;
+    return Math.trunc(answer * 1000);
 }
