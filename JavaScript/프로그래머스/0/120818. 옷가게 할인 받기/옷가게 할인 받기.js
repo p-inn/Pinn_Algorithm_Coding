810de6089f158a4d0b8c9ac9adaf043d97a8fe94
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-16 22:31:35 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ JavaScript/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,11 @@
 function solution(price) {
-    if (price >= 500000) {
-        return Math.floor(price * 0.8);
-    } else if (price >= 300000) {
-        return Math.floor(price * 0.9);
-    } else if (price >= 100000) {
-        return Math.floor(price * 0.95);
-    } else {
-        return price;
-    }
+    var answer = 0;
+    if(price >= 500000){
+        answer = price * 0.8;
+    }else if(price >= 300000){
+        answer = price * 0.9;
+    }else if(price >= 100000){
+        answer = price * 0.95;
+    }else answer = price;
+    return Math.trunc(answer);
 }
