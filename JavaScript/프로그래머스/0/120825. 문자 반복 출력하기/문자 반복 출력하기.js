9ea5c8563323a625e6d1cdbe044466f000990590
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-17 20:47:16 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ JavaScript/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, n) {
-    var answer = '';
-    
-
-    for (let char of my_string) {
-        answer += char.repeat(n);
-    }
-    
-    return answer;
+    return my_string.split('').map(char => char.repeat(n)).join('');
 }
