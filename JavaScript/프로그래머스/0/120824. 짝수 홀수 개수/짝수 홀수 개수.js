688b237905d78b8c865dd1a7e6fd51e95e4d9bb7
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-17 20:45:05 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.17 ms, Memory: 32.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,14 +1,10 @@
 function solution(num_list) {
-    var answer = [];
-    var a = [];
-    var b = [];
-    for(let i=0;i<num_list.length;i++){
-        if(num_list[i]%2!==0){
-            a.push([i]);
-        }else{
-            b.push([i]);
-        }
+    let num = 0;
+    let num1 = 0;
+    for(let i = 0; i< num_list.length; i++){
+        if(num_list[i] % 2 == 0){
+            num++;
+        }else num1++;
     }
-    answer = [b.length, a.length];
-    return answer;
+    return [num, num1];
 }
