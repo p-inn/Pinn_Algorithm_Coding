7af8424881eac9cfa64c8c4ae7a62a2d675a7cb5
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-18 18:28:18 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120829. 각도기/각도기.js
+++ JavaScript/프로그래머스/0/120829. 각도기/각도기.js
@@ -1,14 +1,9 @@
 function solution(angle) {
-    var answer = 0;
-    if (0 < angle && angle < 90) {
-        answer = 1;
-    }else if (angle === 90) {
-        answer = 2;
-    }else if (90 < angle && angle < 180) {
-        answer = 3;
-    }else if(angle===180){
-    answer = 4;
-    }
-    return answer;
+    if(angle < 90){
+        return 1;
+    }else if (angle == 90){
+        return 2;
+    }else if (angle < 180){
+        return 3;
+    }else if (angle == 180)return 4;
 }
-
