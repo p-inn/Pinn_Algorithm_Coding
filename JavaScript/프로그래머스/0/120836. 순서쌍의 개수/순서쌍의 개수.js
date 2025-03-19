1dc198c8c6e4084a5cc099a8cfc54e00442c02df
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-19 19:50:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ JavaScript/프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,8 +1,9 @@
 function solution(n) {
     let count = 0;
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            count++;
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) { 
+            count += 2;
+            if (i === n / i) count--;
         }
     }
     return count;
