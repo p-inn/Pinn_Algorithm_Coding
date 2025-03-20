1dbac7a47b6663212ebfc46a6d45c6ceffd539aa
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-20 23:27:13 +0900

    [level 0] Title: 가위 바위 보, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ JavaScript/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,15 +1,12 @@
 function solution(rsp) {
-    let result = '';
-
-    for (let i = 0; i < rsp.length; i++) {
-        if (rsp[i] === '2') {
-            result += '0'; 
-        } else if (rsp[i] === '0') {
-            result += '5';
-        } else if (rsp[i] === '5') {
-            result += '2'; 
-        }
+    var answer = [];
+    let rspString = rsp.split('');
+    for(let i = 0; i < rspString.length; i++){
+        if (rspString[i] == 2) {
+            answer.push(0);
+        }else if(rspString[i] == 0){
+            answer.push(5);
+        }else if (rspString[i] == 5) answer.push(2);
     }
-
-    return result;
+    return answer.join('');
 }
