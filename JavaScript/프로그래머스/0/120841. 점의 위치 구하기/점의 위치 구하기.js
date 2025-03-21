fb5d86dce430607c74fd9c65028b001bef245340
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-21 23:00:37 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ JavaScript/프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,13 +1,11 @@
 function solution(dot) {
-    const [x, y] = dot;
-
-    if (x > 0 && y > 0) {
-        return 1; 
-    } else if (x < 0 && y > 0) {
-        return 2;  
-    } else if (x < 0 && y < 0) {
-        return 3;  
-    } else {
-        return 4;  
+    var answer = 0;
+    if(dot[0] > 0 && dot[1] > 0 ){
+        return 1;
+    }else if(dot[0] < 0 && dot[1] > 0 ){
+        return 2;
+    }else if(dot[0] < 0 && dot[1] < 0 ){
+        return 3;
     }
+    else return 4;
 }
