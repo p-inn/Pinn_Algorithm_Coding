68f7dac8de033b3a19cbd9e3d2ea3db949d29059
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-21 23:06:03 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ JavaScript/프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,6 +1,6 @@
 function solution(num_list, n) {
-    const answer = [];
-    for (let i = 0; i < num_list.length; i += n) {
+    let answer = [];
+    for(let i = 0; i < num_list.length; i += n){
         answer.push(num_list.slice(i, i + n));
     }
     return answer;
