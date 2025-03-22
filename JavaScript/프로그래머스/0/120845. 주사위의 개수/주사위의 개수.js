0aaafe5d1e7db305bc647ae6115ba42bb12fe56b
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-22 22:19:02 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
+++ JavaScript/프로그래머스/0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,7 +1,6 @@
 function solution(box, n) {
-    const lengthFit = Math.floor(box[0] / n);
-    const widthFit = Math.floor(box[1] / n);
-    const heightFit = Math.floor(box[2] / n);
-    
-    return lengthFit * widthFit * heightFit;
+    let width = Math.floor(box[0] / n);
+    let height = Math.floor(box[1] / n);
+    let high = Math.floor(box[2] / n);
+    return width * height * high;
 }
