1b2f482b297b57c81f657902e69bea121b53dcac
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-24 17:50:14 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
+++ JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 숨어있는 숫자의 덧셈 (1) - 120851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120851) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.06 ms
+메모리: 33.3 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 12일 23:13:47
+2025년 03월 24일 17:50:12
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,14 +1,6 @@
 function solution(my_string) {
-    let sum = 0;
-    
-    for (let i = 0; i < my_string.length; i++) {
-        let char = my_string[i];
-        
-        // 문자가 숫자인지 확인
-        if (!isNaN(char) && char !== ' ') {
-            sum += parseInt(char);
-        }
-    }
-    
-    return sum;
+    return my_string.split('')
+    .filter(char => !isNaN(char) && char !== ' ')
+    .map(Number)
+    .reduce((acc, cur) => acc + cur, 0);
 }
