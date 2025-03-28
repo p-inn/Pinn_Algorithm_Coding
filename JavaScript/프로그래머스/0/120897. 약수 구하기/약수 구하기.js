4f69174ef89e03e8216c8bdedc9e13ff21cf0018
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-28 23:40:51 +0900

    [level 0] Title: 약수 구하기, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ JavaScript/프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,11 +1,9 @@
 function solution(n) {
-    const answer = [];
-
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) { 
-            answer.push(i);
-        }
+  var answer = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      answer.push(i);
     }
-
-    return answer;
+  }
+  return answer;
 }
