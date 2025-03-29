a075c9d568c716c3ba2c183e641483442bf0d7e1
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-29 23:15:27 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
+++ JavaScript/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,3 +1,14 @@
 function solution(my_string) {
-    return eval(my_string);
+  const tokens = my_string.split(' ');
+  let result = +tokens[0];
+
+  for (let i = 1; i < tokens.length; i += 2) {
+    const op = tokens[i];
+    const num = +tokens[i + 1];
+
+    if (op === '+') result += num;
+    else if (op === '-') result -= num;
+  }
+
+  return result;
 }
