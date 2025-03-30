6c26bf401842571f8347cacb55bfa1c9552ec61d
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-30 18:14:07 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ JavaScript/프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,3 +1,9 @@
 function solution(n, numlist) {
-    return numlist.filter(num => num % n === 0);
+    var answer = [];
+    for(let i = 0; i < numlist.length; i++){
+        if(numlist[i] % n == 0 ){
+            answer.push(numlist[i]);
+        }
+    }
+    return answer;
 }
