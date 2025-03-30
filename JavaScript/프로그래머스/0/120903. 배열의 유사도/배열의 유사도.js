46181b884efb72a83cacf4019989695a7f06ba49
Pinn <138990007+p-inn@users.noreply.github.com>
2025-03-30 18:07:11 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ JavaScript/프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,8 +1,9 @@
 function solution(s1, s2) {
-    const set1 = new Set(s1);
-    const set2 = new Set(s2);
-
-    let intersection = new Set([...set1].filter(x => set2.has(x)));
-
-    return intersection.size;
+    var answer = 0;
+    for(let i = 0; i < s1.length; i++){
+        if (s2.includes(s1[i])) {
+            answer += 1;
+        }
+    }
+    return answer;
 }
