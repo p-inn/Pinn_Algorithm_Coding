9d5349d4ac34c4e524d112b4f40d27f144e89323
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-07 21:11:20 +0900

    [level 0] Title: OX퀴즈, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
+++ JavaScript/프로그래머스/0/120907. OX퀴즈/OX퀴즈.js
@@ -1,20 +1,7 @@
 function solution(quiz) {
-    return quiz.map(q => {
-        // 수식을 공백을 기준으로 분리
-        const [X, operator, Y, , Z] = q.split(' ');
-
-        // X와 Y는 정수로 변환
-        const numX = parseInt(X);
-        const numY = parseInt(Y);
-        const numZ = parseInt(Z);
-
-        let result;
-        if (operator === '+') {
-            result = numX + numY;
-        } else if (operator === '-') {
-            result = numX - numY;
-        }
-
-        return result === numZ ? "O" : "X";
-    });
+  return quiz.map(q => {
+    const [X, op, Y, , Z] = q.split(" ");
+    const result = op === "+" ? +X + +Y : +X - +Y;
+    return result === +Z ? "O" : "X";
+  });
 }
