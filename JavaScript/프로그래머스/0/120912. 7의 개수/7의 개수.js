1d03be763e49850321279d30e554e3183e99406b
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-08 23:56:44 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ JavaScript/프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,8 +1,7 @@
 function solution(array) {
-
-    const combinedString = array.join('');
-    
-    const countOfSevens = combinedString.split('7').length - 1;
-    
-    return countOfSevens;
+  return array
+    .join("") 
+    .split("")
+    .filter(c => c === "7")
+    .length;
 }
