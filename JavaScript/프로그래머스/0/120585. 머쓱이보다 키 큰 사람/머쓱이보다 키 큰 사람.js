8e9d0d2dc9c727448d61bcf6f132262e267c85cd
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-09 14:24:44 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ JavaScript/프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,10 +1,9 @@
 function solution(array, height) {
-    let count = 0;
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > height) {
-            count++;
+    var answer = 0;
+    for(let i = 0; i < array.length; i++){
+        if(height < array[i]){
+            answer++;
         }
     }
-    return count;
+    return answer;
 }
-
