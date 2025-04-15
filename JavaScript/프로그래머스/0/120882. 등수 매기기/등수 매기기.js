5a849d42fb1c8f555756b893d2d94e43c2b9e8d4
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-16 00:13:28 +0900

    [level 0] Title: 등수 매기기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ JavaScript/프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,21 +1,6 @@
 function solution(score) {
-
-    const averages = score.map((scores, index) => ({
-        average: (scores[0] + scores[1]) / 2,
-        index: index
-    }));
-
-    averages.sort((a, b) => b.average - a.average);
-
-    const ranks = new Array(score.length).fill(0);
-    let rank = 1;
-
-    for (let i = 0; i < averages.length; i++) {
-        if (i > 0 && averages[i].average < averages[i - 1].average) {
-            rank = i + 1;
-        }
-        ranks[averages[i].index] = rank;
-    }
-
-    return ranks;
+    const averages = score.map(([eng, math]) => (eng + math) / 2);
+    const sorted = [...averages].sort((a, b) => b - a);
+    const answer = averages.map(score => sorted.indexOf(score) + 1);
+    return answer;
 }
