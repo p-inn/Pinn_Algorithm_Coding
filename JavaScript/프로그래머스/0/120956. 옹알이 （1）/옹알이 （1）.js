775303bee63396c7be60c032a8cb576926408d45
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-18 11:00:55 +0900

    [level 0] Title: 옹알이 (1), Time: 0.40 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ JavaScript/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,30 +1,18 @@
 function solution(babbling) {
-  const validWords = ["aya", "ye", "woo", "ma"];
-  let count = 0;
+  const canSpeak = ["aya", "ye", "woo", "ma"];
 
-  babbling.forEach(word => {
-    let temp = word;
-    let prev = "";
-    let isValid = true;
-
-    while (temp.length > 0) {
-      let found = false;
-      for (let valid of validWords) {
-        if (temp.startsWith(valid) && prev !== valid) {
-          temp = temp.slice(valid.length); // 발음을 제거
-          prev = valid; // 이전 발음을 갱신
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        isValid = false;
-        break;
-      }
+  return babbling.filter((word) => {
+    for (let sound of canSpeak) {
+      if (word.includes(sound + sound)) return false;
     }
 
-    if (isValid && temp === "") count++;
-  });
+    for (let sound of canSpeak) {
+      word = word.replaceAll(sound, " ");
+    }
 
-  return count;
+    return word.trim() === "";
+  }).length;
 }
+
+
+
