1d761c913f02ff349a8db9f232e220bf894f5ae9
Pinn <138990007+p-inn@users.noreply.github.com>
2025-04-29 18:08:43 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
+++ JavaScript/프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,9 +1,6 @@
 function solution(bin1, bin2) {
-
-    const num1 = parseInt(bin1, 2);
-    const num2 = parseInt(bin2, 2);
-
-    const answer = num1 + num2;
-
+    let binA = parseInt(bin1, 2);
+    let binB = parseInt(bin2, 2);
+    const answer = binA + binB;
     return answer.toString(2);
 }
