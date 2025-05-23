3de4713de70c3684d96d2b3914937aa0064c233c
Pinn <138990007+p-inn@users.noreply.github.com>
2025-05-23 15:37:48 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ JavaScript/프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,9 +1,11 @@
 function solution(n) {
-    let sum = 0;
+  let sum = 0;
 
-    for (let i = n % 2 === 0 ? 2 : 1; i <= n; i += 2) {
-        sum += n % 2 === 0 ? i * i : i;
-    }
+  for (let i = 1; i <= n; i++) {
+    sum += (n % 2 === 1)
+      ? (i % 2 === 1 ? i : 0)
+      : (i % 2 === 0 ? i * i : 0);
+  }
 
-    return sum;
+  return sum;
 }
