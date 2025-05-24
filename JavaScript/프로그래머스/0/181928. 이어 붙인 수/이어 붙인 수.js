099bfec75814a59d165c9c971e84743e9103470b
Pinn <138990007+p-inn@users.noreply.github.com>
2025-05-25 00:35:13 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
+++ JavaScript/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,5 +1,14 @@
 function solution(num_list) {
-    const sumA = num_list.filter(n => n % 2 === 1).join("");
-    const sumB = num_list.filter(n => n % 2 === 0).join("");
-    return Number(sumA) +Number(sumB);
+  let odd = "";
+  let even = "";
+
+  for (const n of num_list) {
+    if (n % 2 === 0) {
+      even += n;
+    } else {
+      odd += n;
+    }
+  }
+
+  return Number(odd) + Number(even);
 }
