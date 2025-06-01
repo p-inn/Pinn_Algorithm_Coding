75e570ad34376698de32f03249fa977cb3a1b2ac
Pinn <138990007+p-inn@users.noreply.github.com>
2025-06-01 16:17:32 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ JavaScript/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,8 +1,12 @@
 function solution(num_list) {
-    const last = num_list[num_list.length - 1];
-    const secondLast = num_list[num_list.length - 2];
-    
-    num_list.push(last > secondLast ? last - secondLast : last * 2);
-    
-    return num_list;
+  const last = num_list[num_list.length - 1];
+  const prev = num_list[num_list.length - 2];
+
+  if (last > prev) {
+    num_list.push(last - prev);
+  } else {
+    num_list.push(last * 2);
+  }
+
+  return num_list;
 }
