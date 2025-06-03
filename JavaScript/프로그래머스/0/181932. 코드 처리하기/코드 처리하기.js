7734e6272c6ee3e3b3d4b403fa13b12b33205e89
Pinn <138990007+p-inn@users.noreply.github.com>
2025-06-03 23:53:37 +0900

    [level 0] Title: 코드 처리하기, Time: 31.57 ms, Memory: 36.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,16 +1,18 @@
 function solution(code) {
-  let mode = 0;
-  let ret = "";
+    let mode = 0;
+    let ret = [];
 
-  for (let i = 0; i < code.length; i++) {
-    if (code[i] === "1") {
-      mode = 1 - mode;
-    } else {
-      if ((mode === 0 && i % 2 === 0) || (mode === 1 && i % 2 === 1)) {
-        ret += code[i];
-      }
+    for (let i = 0; i < code.length; i++) {
+        const ch = code[i];
+
+        if (ch === '1') {
+            mode = 1 - mode;
+        } else {
+            if ((mode === 0 && i % 2 === 0) || (mode === 1 && i % 2 === 1)) {
+                ret.push(ch);
+            }
+        }
     }
-  }
 
-  return ret.length > 0 ? ret : "EMPTY";
+    return ret.length === 0 ? "EMPTY" : ret.join('');
 }
