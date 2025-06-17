ea82698e95a82174eae2a73b77508ceef29f8f54
Pinn <138990007+p-inn@users.noreply.github.com>
2025-06-18 00:36:37 +0900

    [level 0] Title: 5명씩, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181886. 5명씩/5명씩.js
+++ JavaScript/프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,9 +1,8 @@
 function solution(names) {
     let result = [];
-    
-    for (let i = 0; i < names.length; i += 5) {
-        result.push(names[i]); 
-    }
-    
-    return result;
+  for (let i = 0; i < names.length; i += 5) {
+    result.push(names.slice(i, i + 5));
+  }  
+ const answer = result.map(group => group[0]);
+  return answer;
 }
