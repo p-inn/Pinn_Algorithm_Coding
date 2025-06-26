28000c58dfeb574d58865f8fb1784ffab0cd2215
Pinn <138990007+p-inn@users.noreply.github.com>
2025-06-26 23:44:13 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.05 ms, Memory: 33.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
+++ JavaScript/프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -1,9 +1,6 @@
 function solution(arr, idx) {
-    for (let i = idx; i < arr.length; i++) {
-        if (arr[i] === 1) {
-            return i;
-        }
-    }
-    return -1;
+  for (let i = idx; i < arr.length; i++) {
+    if (arr[i] === 1) return i;
+  }
+  return -1;
 }
-
