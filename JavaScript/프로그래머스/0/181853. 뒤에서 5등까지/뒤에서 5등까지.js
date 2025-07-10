91fea7353883b0c2f3cbecb39d457c6c389d88ca
Pinn <138990007+p-inn@users.noreply.github.com>
2025-07-10 21:05:32 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
+++ JavaScript/프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -1,7 +1,4 @@
 function solution(num_list) {
-
-    num_list.sort((a, b) => a - b);
-    
-
-    return num_list.slice(0, 5);
+    num_list = num_list.sort((a, b) => a - b);
+    return num_list.slice(0,5);
 }
