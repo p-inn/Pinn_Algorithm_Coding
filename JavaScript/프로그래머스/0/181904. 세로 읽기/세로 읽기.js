ac0057078719a14b79e3e40df73a1ed291624ad4
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-01 00:13:41 +0900

    [level 0] Title: 세로 읽기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ JavaScript/프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,7 +1,7 @@
 function solution(my_string, m, c) {
-    let result = '';
-    for (let i = c - 1; i < my_string.length; i += m) {
-        result += my_string[i];
-    }
-    return result;
+  return my_string
+    .split("")
+    .slice(0, Math.ceil(my_string.length / m))
+    .map((_, idx) => my_string.charAt(idx * m + c - 1))
+    .join("");
 }
