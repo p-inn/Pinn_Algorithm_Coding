a558f6a11686abc395a8f6da7c055a34b6d47fa6
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-02 22:05:41 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ JavaScript/프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,4 @@
 function solution(num_list) {
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] < 0) {
-            return i;
-        }
-    }
-    return -1;
+    var answer = 0;
+    return num_list.findIndex(num => num < 0);;
 }
