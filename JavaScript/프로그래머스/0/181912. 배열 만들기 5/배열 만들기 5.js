48942a7201cc81493db59daf8397463f73111ff3
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-05 20:54:11 +0900

    [level 0] Title: 배열 만들기 5, Time: 3.10 ms, Memory: 39.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ JavaScript/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,12 +1,4 @@
 function solution(intStrs, k, s, l) {
-    let result = [];
-
-    for (let str of intStrs) {
-        let slicedNum = parseInt(str.slice(s, s + l));
-        if (slicedNum > k) {
-            result.push(slicedNum);
-        }
-    }
-
-    return result;
+    const answer = intStrs.map((c) => Number(c.slice(s, s + l)));
+    return answer.filter(a => a > k);
 }
