20757fe5370685ac9aeebb97fa24e9b5e40447a3
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-13 23:14:08 +0900

    [level 0] Title: 접미사 배열, Time: 0.21 ms, Memory: 33.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ JavaScript/프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,12 +1,3 @@
 function solution(my_string) {
-    let answer = [];
-
-    for (let i = 0; i < my_string.length; i++) {
-        answer.push(my_string.slice(i));
-    }
-    
-
-    answer.sort();
-    
-    return answer;
+    return [...my_string].map((_, i) => my_string.slice(i)).sort();
 }
