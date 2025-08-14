1004098a487e9c47d9d6cdf0118e135b19bbfbd7
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-14 18:07:31 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ JavaScript/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,5 +1,3 @@
 function solution(num_list, n) {
-    const afterN = num_list.slice(n);
-    const beforeN = num_list.slice(0, n);
-    return afterN.concat(beforeN);
+    return num_list.slice(n).concat(num_list.slice(0, n));
 }
