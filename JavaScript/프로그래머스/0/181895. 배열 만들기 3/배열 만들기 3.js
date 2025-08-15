22142b5bf59493e08c8b58fa6bbde0ab36b005cb
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-15 22:07:46 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.86 ms, Memory: 41.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ JavaScript/프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -1,6 +1,5 @@
 function solution(arr, intervals) {
-    let firstInterval = arr.slice(intervals[0][0], intervals[0][1] + 1);
-    let secondInterval = arr.slice(intervals[1][0], intervals[1][1] + 1);
-    
-    return firstInterval.concat(secondInterval);
+    let arr1 = arr.slice(intervals[0][0], intervals[0][1] + 1);
+    let arr2 = arr.slice(intervals[1][0], intervals[1][1] + 1);
+    return arr1.concat(arr2);
 }
