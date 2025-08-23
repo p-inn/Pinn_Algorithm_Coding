cebfffca77b7035e396ca91a713025d62a4dc94f
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-23 23:48:33 +0900

    [level 0] Title: 배열 만들기 6, Time: 9.81 ms, Memory: 63.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ JavaScript/프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -1,17 +1,13 @@
 function solution(arr) {
-    let stk = [];
-    let i = 0;
-
-    while (i < arr.length) {
-        if (stk.length === 0) {
+    var stk = [];
+    for(let i = 0; i < arr.length; i++){
+        if(stk.length == 0){
             stk.push(arr[i]);
-        } else if (stk[stk.length - 1] === arr[i]) {
+        }else if(stk.length > 0 && stk[stk.length - 1] == arr[i]){
             stk.pop();
-        } else {
+        }else {
             stk.push(arr[i]);
         }
-        i++;
     }
-
-    return stk.length === 0 ? [-1] : stk;
+    return stk.length == 0 ? [-1] : stk;
 }
