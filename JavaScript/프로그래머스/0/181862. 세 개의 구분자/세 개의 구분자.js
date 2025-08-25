98909b6bffbbd112c8d2a3d99ac95ede9ab0ff00
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-25 23:34:17 +0900

    [level 0] Title: 세 개의 구분자, Time: 29.18 ms, Memory: 61.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ JavaScript/프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,5 +1,4 @@
 function solution(myStr) {
-    const result = myStr.split(/[abc]/).filter(segment => segment !== "");
-
-    return result.length > 0 ? result : ["EMPTY"];
+    const parts = myStr.split(/[abc]/).filter(Boolean);
+    return parts.length ? parts : ["EMPTY"];
 }
