22e323bde8fb812374ce89f05be3e9406a432eca
Pinn <138990007+p-inn@users.noreply.github.com>
2025-08-31 00:32:16 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.13 ms, Memory: 33.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ JavaScript/프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,8 +1,5 @@
 function solution(date1, date2) {
-    const d1 = new Date(date1[0], date1[1] - 1, date1[2]); // 월은 0부터 시작하므로 -1
-    const d2 = new Date(date2[0], date2[1] - 1, date2[2]);
-    if(d1<d2){
-        return 1;
-    }
-    return 0;
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return d1 < d2 ? 1 : 0;
 }
