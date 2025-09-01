714be04ac12d42e94b537b038af47beeb8982ab3
Pinn <138990007+p-inn@users.noreply.github.com>
2025-09-01 20:48:06 +0900

    [level 0] Title: qr code, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181903. qr code/qr code.js
+++ JavaScript/프로그래머스/0/181903. qr code/qr code.js
@@ -1,8 +1,9 @@
 function solution(q, r, code) {
     var answer = '';
-    for(let i=0;i<code.length; i++){
-        if(i % q === r){
-            answer += code[i]; 
+    code = code.split('');
+    for(let i = 0; i < code.length; i++){
+        if(i % q ==r) {
+            answer+=(code[i]);
         }
     }
     return answer;
