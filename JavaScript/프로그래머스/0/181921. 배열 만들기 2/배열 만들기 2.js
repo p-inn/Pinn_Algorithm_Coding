b61b47f034a481a535c0bcc87ff4ab8737c9dc92
Pinn <138990007+p-inn@users.noreply.github.com>
2025-09-05 23:50:50 +0900

    [level 0] Title: 배열 만들기 2, Time: 91.70 ms, Memory: 52.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ JavaScript/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,10 +1,11 @@
 function solution(l, r) {
-    var answer = [];
-    for(let i = l; i <= r; i++){
-        const isValid = [...String(i)].every(char => ['0', '5'].includes(char));
-        if(isValid == true){
-            answer.push(i);
-        }
+  const result = [];
+  
+  for (let i = l; i <= r; i++) {
+    if (/^[05]+$/.test(String(i))) {
+      result.push(i);
     }
-    return answer.length === 0 ? [-1] : answer;
+  }
+  
+  return result.length ? result : [-1];
 }
