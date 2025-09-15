94a9845c3c109444048c9fa7a5d673e1fe24638e
Pinn <138990007+p-inn@users.noreply.github.com>
2025-09-16 00:38:03 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ JavaScript/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,14 +1,12 @@
 function solution(n) {
-    var answer = [n];
-    const sequence = [n];
-      while (n !== 1) {
-        if (n % 2 === 0) {
-          n = n / 2;
-        } else {
-          n = 3 * n + 1;
-        }
-        answer.push(n);
-      }
-      return answer;
+  const result = [n]; 
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
     }
-    
+    result.push(n);
+  }
+  return result;
+}
