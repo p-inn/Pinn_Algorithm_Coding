d3a12de8973407fcaeb879f599c63e53bac0d4b8
Pinn <138990007+p-inn@users.noreply.github.com>
2025-09-17 21:20:48 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.46 ms, Memory: 33.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ JavaScript/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,6 +1,8 @@
 function solution(arr, queries) {
-  for (let [i, j] of queries) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  for (const query of queries) {
+    const i = query[0];
+    const j = query[1];
+    [arr[i], arr[j]] = [arr[j], arr[i]]; 
   }
   return arr;
 }
