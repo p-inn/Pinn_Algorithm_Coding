3bf23316a67daea8b9b8625cfec8c0d5622e5413
Pinn <138990007+p-inn@users.noreply.github.com>
2025-09-21 23:18:21 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ JavaScript/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,9 +1,12 @@
 function solution(rank, attendance) {
-    return rank
+
+  const candidates = rank
     .map((r, i) => [r, i])
-    .filter(([_, i]) => attendance[i])
-    .sort((a, b) => a[0] - b[0])
-    .slice(0, 3)
-    .map(([_, i]) => i)
-    .reduce((acc, v, idx) => acc + v * [10000, 100, 1][idx], 0);
+    .filter(([_, i]) => attendance[i]);
+
+  candidates.sort((a, b) => a[0] - b[0]);
+
+  const [a, b, c] = candidates.slice(0, 3).map(([_, i]) => i);
+
+  return 10000 * a + 100 * b + c;
 }
