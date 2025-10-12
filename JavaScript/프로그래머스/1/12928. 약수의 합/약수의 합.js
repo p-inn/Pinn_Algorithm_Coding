53b9b6d3a3716838d21e0f7404360e7116c3b1c5
Pinn <138990007+p-inn@users.noreply.github.com>
2025-10-12 23:59:36 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,7 @@
+function solution(n) {
+  const divisors = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) divisors.push(i);
+  }
+  return divisors.reduce((acc, cur) => acc + cur, 0);
+}
